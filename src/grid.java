5271d23e0f740cd9db3f87f7da3989e01103472c
braydenleung-Git <brayden.leung@gmail.com>
2024-03-28 12:01:25 -0400

    created location.java and print grind methods

--- src/grid.java
+++ src/grid.java
@@ -1,2 +1,17 @@
 public class grid {
+
+    private final int colSize = 9; //column size
+    private final int rowSize = 9; //row size
+    location[][] mainGrid = new location[colSize][rowSize];
+
+    public void printGrid(){
+        System.out.println("1 2 3 4 5 6 7 8 9");
+        for (int vert = 0; vert < rowSize; vert++) {
+            System.out.print(vert+1);
+            for (int hori = 0; hori < colSize; hori++) {
+                System.out.print(mainGrid[vert][hori].getInt());
+            }
+            System.out.println();
+        }
+    }
 }
