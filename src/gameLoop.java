6b7ec613438d69fe5fb0e52de40ed9be3ee8d9d1
Tidal_Maze <tidalmaze0432@gmail.com>
2024-04-08 22:07:17 -0400

    updated the interfacing class :)

---
+++ src/gameLoop.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LoopGame
+{
+
+
+    public static void main()
+    {
+        //implement input method here
+        //this is placed at the very beginning of the game; the initial input is set to "YES" until the replay question was asked after the game
+        while(!(input.euqals("NO")){
+        System.out.println("Do you wish to play again?");
+        if(/*insert object name*/.readInput().equals("YES")){
+            //this is where the entire code of the game fits
+        }
+            else{
+            System.out.println("We hope you had a fun time playing! Bye for now!");
+            break;
+        }
+
+    }
---
+++ src/userInput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class UserInput
+{
+
+
+    public static void readInput()
+    {
+        String input = "";
+        Scanner s1 = new Scanner(System.in);
+        input = s1.nextLine();
+        input = input.toUpperCase();
+    }
+}
